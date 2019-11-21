1aab20f2d861f0738403eae3dae17607f2a497ab
leocabanig154 <leeleccion@gmail.com>
2019-11-21 14:26:02 +0800

    model view controller

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::get('/students', "StudentController@index")->name("students.index");
