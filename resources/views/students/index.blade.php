fd9636de441fbe70f3cb96c1793aaee62955e230
leocabanig154 <leeleccion@gmail.com>
2019-11-21 14:26:34 +0800

    model view controller

---
+++ resources/views/students/index.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+        	<h1>Students List here</h1>
+        	<table class="table">
+        		<thead>
+        			<tr>
+        				<th>#</th>
+        				<th>Name</th>
+        			</tr>	
+        		</thead>
+        		<tbody>
+        			<tr>
+        			  <td>1.</td>
+        			  <td>Leo Philip Cabanig</td>
+        			</tr>
+        			<tr>
+        			  <td>2.</td>
+        			  <td>Lea Sienes</td>
+        			</tr>
+        			<tr>
+        			  <td>3.</td>
+        			  <td>Janin Marimon</td>
+        			</tr>
+        		<tbody>	
+        	</table>			
+        </div>
+    </div>
+</div>
+@endsection
+
