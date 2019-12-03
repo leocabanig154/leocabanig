1c942d18beb122719aba58d7b19d2a8d3cf356eb
leocabanig154 <leeleccion@gmail.com>
2019-12-03 14:12:42 +0800

    video1-9

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -5,7 +5,8 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
         	<h1>Students List here</h1>
-        	<a class="btn btn-primary" href="{{ route('students.add')}}">Add Student</a>
+        	<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#add-student-modal">Add Student</button>
+        	@include('students.modals._add-student-modal')
         	<table class="table">
         		<thead>
         			<tr>
